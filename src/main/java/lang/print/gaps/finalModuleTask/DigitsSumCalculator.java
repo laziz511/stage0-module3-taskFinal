788d62dfe9b365332aa74ec11a9cfd8adb9887c7
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-22 10:39:07 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
-        int  result =(number/1000 + number%10 + (number/100)%10 + (number%10)%10);
+        int  result =(number/1000 + number%10 + (number/100)%10 + (number/10)%10);
         System.out.println(result);
     }
 }
