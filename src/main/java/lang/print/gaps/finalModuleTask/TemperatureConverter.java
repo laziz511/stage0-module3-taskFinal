97081ff047f4f0013b95b71c2f1fb4df921bfa0f
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-22 10:21:31 +0500

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double Fahrenheit = (1.8*temperatureCelsius + 32);
+        System.out.println(Fahrenheit);
     }
 }
